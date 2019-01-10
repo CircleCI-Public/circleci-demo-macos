b7a642b53436dfca239228aefb38a708f2d7ae1a
teesloane <tylersloane@gmail.com>
2019-01-10 15:56:54 -0500

    Initial Commit

---
+++ circleci-demo-macos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  circleci-demo-macos
+//
+//  Created by Tyler Sloane on 2019-01-10.
+//  Copyright © 2019 your-org. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ circleci-demo-macosTests/circleci_demo_macosTests.swift
@@ -0,0 +1,34 @@
+//
+//  circleci_demo_macosTests.swift
+//  circleci-demo-macosTests
+//
+//  Created by Tyler Sloane on 2019-01-10.
+//  Copyright © 2019 your-org. All rights reserved.
+//
+
+import XCTest
+@testable import circleci_demo_macos
+
+class circleci_demo_macosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
